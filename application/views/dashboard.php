17d43573fc62b095475077f6c33f568a19cdd5ce
rarangs <raka07185@gmail.com>
2023-04-26 21:32:15 +0700

    dashboard

---
+++ application/views/dashboard.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php $this->load->view('partials/head.php') ?>
+  </head>
+           <?php $this->load->view('partials/sidebar.php') ?>
+
+        <!-- top navigation -->
+       <?php $this->load->view('partials/topbar.php') ?>
+        <!-- /top navigation -->
+
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <!-- top tiles -->
+            <h2><center>  </center></h2>
+          <!-- /top tiles -->
+
+          <div class="row">
+
+
+            <div class="col-md-4 col-sm-4 ">
+              <div class="x_panel tile fixed_height_300">
+                <div class="x_title">
+                  <h2>Last Quotation :321</h2>
+                  <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                  <h4><center>  </center></h4>
+                    <div class="clearfix"></div>
+                  </div>
+                </div>
+              </div>
+
+            <div class="col-md-4 col-sm-4 ">
+              <div class="x_panel tile fixed_height_300">
+                <div class="x_title">
+                  <h2>Pendapatan Bulan Ini</h2>
+                  <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                  <h4><center>Rp. 200.000</center></h4>
+                    <div class="clearfix"></div>
+                  </div>
+              </div>
+            </div>
+
+            <div class="col-md-4 col-sm-4 ">
+              <div class="x_panel tile fixed_height_300">
+                <div class="x_title">
+                  <h2>Pendapatan Tahun Ini</h2>
+                  <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                  <h4><center>Rp. 200.000</center></h4>
+                    <div class="clearfix"></div>
+                  </div>
+              </div>
+            </div>
+
+          </div>
+          <br />
+
+          <div class="row">
+            <div class="col-md-12 col-sm-12 ">
+              <div class="dashboard_graph">
+
+                <div class="row x_title">
+                  <div class="col-md-6">
+                    <h3>Informasi</h3>
+                  </div>
+                </div>
+                <diiv class="x_content">
+                  <h4><i class="red">Stok Habis Segera Order!!</i></h4>
+                  <h4>- Shampo Matrix 1</h4>
+                  <h4>- Conditioner Matri 1x</h4>
+                    <div class="clearfix"></div>
+                  </diiv>
+                <div class="clearfix"></div>
+              </div>
+            </div>
+
+          </div>
+          <br />
+
+          <div class="row">
+            <div class="col-md-12 col-sm-12 ">
+              <div class="dashboard_graph">
+
+                <div class="row x_title">
+                  <div class="col-md-6">
+                    <h3>Riwayat Transaksi</h3>
+                  </div>
+                </div>
+                <diiv class="x_content">
+                  <h4><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis repellendus ipsum quasi illum, et at id reprehenderit magni architecto natus quos in assumenda. Necessitatibus rerum officia ipsam pariatur? Eum, dignissimos.</p></h4>
+                  <h4><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis repellendus ipsum quasi illum, et at id reprehenderit magni architecto natus quos in assumenda. Necessitatibus rerum officia ipsam pariatur? Eum, dignissimos.</p></h4>
+                  <h4><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis repellendus ipsum quasi illum, et at id reprehenderit magni architecto natus quos in assumenda. Necessitatibus rerum officia ipsam pariatur? Eum, dignissimos.</p></h4>
+                  <h4><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis repellendus ipsum quasi illum, et at id reprehenderit magni architecto natus quos in assumenda. Necessitatibus rerum officia ipsam pariatur? Eum, dignissimos.</p></h4>
+                  <h4><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis repellendus ipsum quasi illum, et at id reprehenderit magni architecto natus quos in assumenda. Necessitatibus rerum officia ipsam pariatur? Eum, dignissimos.</p></h4>
+                    <div class="clearfix"></div>
+                  </diiv>
+                <div class="clearfix"></div>
+              </div>
+            </div>
+
+          </div>
+          <br />
+          
+        </div>
+        <!-- /page content -->
+
+        <!-- footer content -->
+        <?php $this->load->view('partials/footer.php') ?>
+        <!-- /footer content -->
+      </div>
+    </div>
+    <?php $this->load->view('partials/js.php') ?>
+  </body>
+</html>
